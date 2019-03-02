3d3cc5753a3b24a44b34859e5e888277e3d2a51b
Ark <target.ark@gmail.com>
2019-03-02 19:16:27 +0900

    Update sed code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ fi
 export HOME=/home/user
 
 latexmk -pvc main 2>&1 \
-  | sed -uE -e '/^!/!bend;s/^.*$/\x1b[30;41m&\x1b[0m/;:loop;N;/(\nNo pages of output.)|(\nOutput written)/bend;s/\n([^\n]*$)/\n\x1b[31m\1\x1b[0m/;bloop;:end;P;D' \
+  | sed -uE -e '/^!/!bend;s/^.*$/\x1b[30;41m&\x1b[0m/;:loop;N;/(\nNo pages of output.)|(\nOutput written)|(\n! )|(\nLatexmk:)/bend;s/\n([^\n]*$)/\n\x1b[31m\1\x1b[0m/;bloop;:end;P;D' \
   | sed -uE  -e '/(^LaTeX Warning:)|(^LaTeX Font Warning:)|(^Package babel Warning:)|(^Runaway argument\?)|(^Underfull \\hbox)|(^Overfull \\hbox)|(^Underfull \\vbox)|(^Overfull \\vbox)/s/^.*$/\x1b[33m&\x1b[0m/' \
   | sed -u  -e '/^Running /s/^.*$/\x1b[0m&\x1b[0m/' \
   | sed -uE -e '/^Latexmk: /!bend;s/^.*$/\x1b[0m&\x1b[0m/;:loop;N;/\n  [^\n]*$/s/\n([^\n]*$)/\n\x1b[34m\1\x1b[0m/;tloop;:end;P;D' \
